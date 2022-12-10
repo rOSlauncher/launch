f058144f0b0ddad6590947cf41240cbf1682e51c
rOSlauncher <119766885+rOSlauncher@users.noreply.github.com>
2022-12-09 23:17:32 -0500

    Update projectdeltalaunch.lua

--- projectdeltalaunch.lua
+++ projectdeltalaunch.lua
@@ -1,5 +1,8 @@
-game.CoreGui.rOS.Base.rOSstore["ProjectDelta"].Enabled.Visible = true
-
-game.CoreGui.rOS.Base.rOSstore["ProjectDelta"].MouseButton1Down:connect(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/rOSlauncher/launch/main/projectdelta'))()
+game:GetService'RunService'.RenderStepped:connect(function()
+        if game.CoreGui:FindFirstChild("rOS") then
+           game.CoreGui.rOS.Base.rOSstore["ProjectDelta"].Enabled.Visible = true
+           game.CoreGui.rOS.Base.rOSstore["ProjectDelta"].MouseButton1Down:connect(function()
+         loadstring(game:HttpGet('https://raw.githubusercontent.com/rOSlauncher/launch/main/projectdelta'))()
+        end)
+    end
 end)
